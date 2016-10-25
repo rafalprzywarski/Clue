d770260cab37d80147ac4439f92b1ea82689ac60
Rafal Przywarski <rafal.przywarski@gmail.com>
2016-10-25 11:16:54 -0400

    namespaces are resolved during compilation

--- lua/compiler.lua
+++ lua/compiler.lua
@@ -4,31 +4,31 @@ require 'reader'
 clue = clue or {}
 clue.compiler = clue.compiler or {}
 clue.compiler.special_forms = {
-    fn = function(locals, params, ...)
+    fn = function(ns, locals, params, ...)
         local param_names = clue.map_array(function(s) return s.name end, params.value)
         locals = clue.set_union(locals, clue.to_set(param_names))
         local translated = {}
         for i = 1, select("#", ...) do
-            table.insert(translated, clue.compiler.translate_expr(locals, select(i, ...)))
+            table.insert(translated, clue.compiler.translate_expr(ns, locals, select(i, ...)))
         end
         translated[#translated] = "return " .. translated[#translated]
         return "(function(" .. table.concat(param_names, ", ") .. ") " .. table.concat(translated, "; ") .. " end)"
     end,
-    def = function(locals, sym, value)
-        return "clue._ns_[\"" .. sym.name .. "\"] = " .. clue.compiler.translate_expr({}, value)
+    def = function(ns, locals, sym, value)
+        return "clue.namespaces[\"" .. ns .. "\"][\"" .. sym.name .. "\"] = " .. clue.compiler.translate_expr(ns, {}, value)
     end,
-    let = function(locals, defs, ...)
+    let = function(ns, locals, defs, ...)
         if select("#", ...) == 0 and #defs.value == 0 then
             return "clue.nil_"
         end
         local translated = {}
         local locals = clue.set_union(locals, {})
         for i = 1, #defs.value, 2 do
-            table.insert(translated, "local " .. defs.value[i].name .. " = " .. clue.compiler.translate_expr(locals, defs.value[i + 1]))
+            table.insert(translated, "local " .. defs.value[i].name .. " = " .. clue.compiler.translate_expr(ns, locals, defs.value[i + 1]))
             locals[defs.value[i].name] = true
         end
         for i = 1, select("#", ...) do
-            table.insert(translated, clue.compiler.translate_expr(locals, select(i, ...)))
+            table.insert(translated, clue.compiler.translate_expr(ns, locals, select(i, ...)))
         end
         if select("#", ...) == 0 then
             table.insert(translated, "clue.nil_")
@@ -38,55 +38,55 @@ clue.compiler.special_forms = {
     end
 }
 
-function clue.compiler.translate_call(locals, fn, ...)
+function clue.compiler.translate_call(ns, locals, fn, ...)
     if fn.type == "symbol" and fn.ns == nil and clue.compiler.special_forms[fn.name] then
-        return clue.compiler.special_forms[fn.name](locals, ...)
+        return clue.compiler.special_forms[fn.name](ns, locals, ...)
     end
-    local s = clue.compiler.translate_expr(locals, fn) .. "(";
+    local s = clue.compiler.translate_expr(ns, locals, fn) .. "(";
     for i = 1, select("#", ...) do
         if i > 1 then s = s .. ", " end
-        s = s .. clue.compiler.translate_expr(locals, select(i, ...))
+        s = s .. clue.compiler.translate_expr(ns, locals, select(i, ...))
     end
     return s .. ")"
 end
 
-function clue.compiler.translate_vector(locals, vector)
+function clue.compiler.translate_vector(ns, locals, vector)
     local translated = {}
     for _, v in ipairs(vector) do
-        table.insert(translated, clue.compiler.translate_expr(locals, v))
+        table.insert(translated, clue.compiler.translate_expr(ns, locals, v))
     end
     return "{" .. table.concat(translated, ", ").. "}"
 end
 
-function clue.compiler.translate_expr(locals, expr)
+function clue.compiler.translate_expr(ns, locals, expr)
     if type(expr) ~= "table" then
         return tostring(expr)
     end
     if expr.type == "list" then
-        return clue.compiler.translate_call(locals, unpack(expr.value))
+        return clue.compiler.translate_call(ns, locals, unpack(expr.value))
     elseif expr.type == "symbol" then
         if not expr.ns then
             if locals[expr.name] then
                 return expr.name
             end
-            return "clue._ns_[\"" .. expr.name .. "\"]"
+            return "clue.namespaces[\"" .. ns .. "\"][\"" .. expr.name .. "\"]"
         end
-        return "clue.var(\"" .. expr.ns .. "\", \"" .. expr.name .. "\")"
+        return "clue.namespaces[\"" .. expr.ns .. "\"][\"" .. expr.name .. "\"]"
     elseif expr.type == "vector" then
-        return clue.compiler.translate_vector(locals, expr.value)
+        return clue.compiler.translate_vector(ns, locals, expr.value)
     else
         error("unexpected expression type")
     end
 end
 
-function clue.compiler.translate(exprs)
+function clue.compiler.translate(ns, exprs)
     local s = ""
     for _, expr in ipairs(exprs) do
-    	s = s .. clue.compiler.translate_expr({}, expr)
+    	s = s .. clue.compiler.translate_expr(ns, {}, expr)
     end
     return s
 end
 
-function clue.compiler.compile(source)
-    return clue.compiler.translate(clue.reader.read(source))
+function clue.compiler.compile(ns, source)
+    return clue.compiler.translate(ns, clue.reader.read(source))
 end
--- lua/compiler_test.lua
+++ lua/compiler_test.lua
@@ -5,59 +5,70 @@ t.describe("clue.compiler", {
     [".compile"] = {
         ["should translate"] = {
             ["lists into function calls"] = function()
-                t.assert_equals(clue.compiler.compile("(hi)"), "clue._ns_[\"hi\"]()")
-                t.assert_equals(clue.compiler.compile("(hi 1 2 3)"), "clue._ns_[\"hi\"](1, 2, 3)")
-                t.assert_equals(clue.compiler.compile("(my.ns/ff)"), "clue.var(\"my.ns\", \"ff\")()")
-                t.assert_equals(clue.compiler.compile("(my.ns/ff 1 2 3)"), "clue.var(\"my.ns\", \"ff\")(1, 2, 3)")
-                t.assert_equals(clue.compiler.compile("(my.ns/ff x s/y)"), "clue.var(\"my.ns\", \"ff\")(clue._ns_[\"x\"], clue.var(\"s\", \"y\"))")
-                t.assert_equals(clue.compiler.compile("((fn [] 10))"), "(function() return 10 end)()")
+                ns = "user.ns"
+                t.assert_equals(clue.compiler.compile(ns, "(hi)"), "clue.namespaces[\"user.ns\"][\"hi\"]()")
+                t.assert_equals(clue.compiler.compile(ns, "(hi 1 2 3)"), "clue.namespaces[\"user.ns\"][\"hi\"](1, 2, 3)")
+                t.assert_equals(clue.compiler.compile(ns, "(my.ns/ff)"), "clue.namespaces[\"my.ns\"][\"ff\"]()")
+                t.assert_equals(clue.compiler.compile(ns, "(my.ns/ff 1 2 3)"), "clue.namespaces[\"my.ns\"][\"ff\"](1, 2, 3)")
+                t.assert_equals(clue.compiler.compile(ns, "(my.ns/ff x s/y)"), "clue.namespaces[\"my.ns\"][\"ff\"](clue.namespaces[\"user.ns\"][\"x\"], clue.namespaces[\"s\"][\"y\"])")
+                t.assert_equals(clue.compiler.compile(ns, "((fn [] 10))"), "(function() return 10 end)()")
             end,
             ["nested list into nested function calls"] = function()
-                t.assert_equals(clue.compiler.compile("(hi (there))"), "clue._ns_[\"hi\"](clue._ns_[\"there\"]())")
+                ns = "user.ns"
+                t.assert_equals(clue.compiler.compile(ns, "(hi (there))"), "clue.namespaces[\"user.ns\"][\"hi\"](clue.namespaces[\"user.ns\"][\"there\"]())")
             end,
             ["symbols into vars"] = function()
-                t.assert_equals(clue.compiler.compile("an-example"), "clue._ns_[\"an-example\"]")
-                t.assert_equals(clue.compiler.compile("my.ns.example/an-example"), "clue.var(\"my.ns.example\", \"an-example\")")
+                ns = "user.ns"
+                t.assert_equals(clue.compiler.compile(ns, "an-example"), "clue.namespaces[\"user.ns\"][\"an-example\"]")
+                t.assert_equals(clue.compiler.compile(ns, "my.ns.example/an-example"), "clue.namespaces[\"my.ns.example\"][\"an-example\"]")
             end,
             ["vectors into arrays"] = function()
-                t.assert_equals(clue.compiler.compile("[]"), "{}")
-                t.assert_equals(clue.compiler.compile("[1 2 3 4]"), "{1, 2, 3, 4}")
-                t.assert_equals(clue.compiler.compile("[(hello) s/x]"), "{clue._ns_[\"hello\"](), clue.var(\"s\", \"x\")}")
+                ns = "user.ns"
+                t.assert_equals(clue.compiler.compile(ns, "[]"), "{}")
+                t.assert_equals(clue.compiler.compile(ns, "[1 2 3 4]"), "{1, 2, 3, 4}")
+                t.assert_equals(clue.compiler.compile(ns, "[(hello) s/x]"), "{clue.namespaces[\"user.ns\"][\"hello\"](), clue.namespaces[\"s\"][\"x\"]}")
             end,
             ["function definitions"] = {
                 ["with no parameters"] = function()
-                    t.assert_equals(clue.compiler.compile("(fn [] (f 1 2))"), "(function() return clue._ns_[\"f\"](1, 2) end)")
-                    t.assert_equals(clue.compiler.compile("(fn [] (f 1) (g 2) (h 3))"), "(function() clue._ns_[\"f\"](1); clue._ns_[\"g\"](2); return clue._ns_[\"h\"](3) end)")
+                    ns = "user.ns"
+                    t.assert_equals(clue.compiler.compile(ns, "(fn [] (f 1 2))"), "(function() return clue.namespaces[\"user.ns\"][\"f\"](1, 2) end)")
+                    t.assert_equals(clue.compiler.compile(ns, "(fn [] (f 1) (g 2) (h 3))"), "(function() clue.namespaces[\"user.ns\"][\"f\"](1); clue.namespaces[\"user.ns\"][\"g\"](2); return clue.namespaces[\"user.ns\"][\"h\"](3) end)")
                 end,
                 ["with declared parameters"] = function()
-                    t.assert_equals(clue.compiler.compile("(fn [a] (f 1 2))"), "(function(a) return clue._ns_[\"f\"](1, 2) end)")
-                    t.assert_equals(clue.compiler.compile("(fn [b c d] (f 1 2))"), "(function(b, c, d) return clue._ns_[\"f\"](1, 2) end)")
+                    ns = "user.ns"
+                    t.assert_equals(clue.compiler.compile(ns, "(fn [a] (f 1 2))"), "(function(a) return clue.namespaces[\"user.ns\"][\"f\"](1, 2) end)")
+                    t.assert_equals(clue.compiler.compile(ns, "(fn [b c d] (f 1 2))"), "(function(b, c, d) return clue.namespaces[\"user.ns\"][\"f\"](1, 2) end)")
                 end,
                 ["with parameters used in the body"] = function()
-                    t.assert_equals(clue.compiler.compile("(fn [a] (a 1 2))"), "(function(a) return a(1, 2) end)")
-                    t.assert_equals(clue.compiler.compile("(fn [f x] (f x y))"), "(function(f, x) return f(x, clue._ns_[\"y\"]) end)")
-                    t.assert_equals(clue.compiler.compile("(fn [a b c] (a b) [a b c])"), "(function(a, b, c) a(b); return {a, b, c} end)")
+                    ns = "user.ns"
+                    t.assert_equals(clue.compiler.compile(ns, "(fn [a] (a 1 2))"), "(function(a) return a(1, 2) end)")
+                    t.assert_equals(clue.compiler.compile(ns, "(fn [f x] (f x y))"), "(function(f, x) return f(x, clue.namespaces[\"user.ns\"][\"y\"]) end)")
+                    t.assert_equals(clue.compiler.compile(ns, "(fn [a b c] (a b) [a b c])"), "(function(a, b, c) a(b); return {a, b, c} end)")
                 end,
                 ["with parameters used in the body of a nested function"] = function()
-                    t.assert_equals(clue.compiler.compile("(fn [a b] (fn [c d] (a b c d)))"), "(function(a, b) return (function(c, d) return a(b, c, d) end) end)")
+                    ns = "user.ns"
+                    t.assert_equals(clue.compiler.compile(ns, "(fn [a b] (fn [c d] (a b c d)))"), "(function(a, b) return (function(c, d) return a(b, c, d) end) end)")
                 end
             },
             ["variable definitions"] = function()
-                t.assert_equals(clue.compiler.compile("(def a 10)"), "clue._ns_[\"a\"] = 10")
-                t.assert_equals(clue.compiler.compile("(def ready? (fn [x] x))"), "clue._ns_[\"ready?\"] = (function(x) return x end)")
+                ns = "user.ns"
+                t.assert_equals(clue.compiler.compile(ns, "(def a 10)"), "clue.namespaces[\"user.ns\"][\"a\"] = 10")
+                t.assert_equals(clue.compiler.compile(ns, "(def ready? (fn [x] x))"), "clue.namespaces[\"user.ns\"][\"ready?\"] = (function(x) return x end)")
             end,
             ["let definitions"] = {
                 ["without constants"] = function()
-                    t.assert_equals(clue.compiler.compile("(let [])"), "clue.nil_")
-                    t.assert_equals(clue.compiler.compile("(let [] (f 1 2))"), "(function() return clue._ns_[\"f\"](1, 2) end)()")
-                    t.assert_equals(clue.compiler.compile("(let [] (f) (f 1 2))"), "(function() clue._ns_[\"f\"](); return clue._ns_[\"f\"](1, 2) end)()")
+                    ns = "user.ns"
+                    t.assert_equals(clue.compiler.compile(ns, "(let [])"), "clue.nil_")
+                    t.assert_equals(clue.compiler.compile(ns, "(let [] (f 1 2))"), "(function() return clue.namespaces[\"user.ns\"][\"f\"](1, 2) end)()")
+                    t.assert_equals(clue.compiler.compile(ns, "(let [] (f) (f 1 2))"), "(function() clue.namespaces[\"user.ns\"][\"f\"](); return clue.namespaces[\"user.ns\"][\"f\"](1, 2) end)()")
                 end,
                 ["with constants"] = function()
-                    t.assert_equals(clue.compiler.compile("(let [a (f)])"), "(function() local a = clue._ns_[\"f\"](); return clue.nil_ end)()")
-                    t.assert_equals(clue.compiler.compile("(let [a f b 2] (a b))"), "(function() local a = clue._ns_[\"f\"]; local b = 2; return a(b) end)()")
-                    t.assert_equals(clue.compiler.compile("(let [a 1 b 2] (a) (b))"), "(function() local a = 1; local b = 2; a(); return b() end)()")
-                    t.assert_equals(clue.compiler.compile("(fn [a] (let [b a] (b a)))"), "(function(a) return (function() local b = a; return b(a) end)() end)")
-                    t.assert_equals(clue.compiler.compile("(let [a a a a a b b a] (b a)"), "(function() local a = clue._ns_[\"a\"]; local a = a; local a = clue._ns_[\"b\"]; local b = a; return b(a) end)()")
+                    ns = "user.ns"
+                    t.assert_equals(clue.compiler.compile(ns, "(let [a (f)])"), "(function() local a = clue.namespaces[\"user.ns\"][\"f\"](); return clue.nil_ end)()")
+                    t.assert_equals(clue.compiler.compile(ns, "(let [a f b 2] (a b))"), "(function() local a = clue.namespaces[\"user.ns\"][\"f\"]; local b = 2; return a(b) end)()")
+                    t.assert_equals(clue.compiler.compile(ns, "(let [a 1 b 2] (a) (b))"), "(function() local a = 1; local b = 2; a(); return b() end)()")
+                    t.assert_equals(clue.compiler.compile(ns, "(fn [a] (let [b a] (b a)))"), "(function(a) return (function() local b = a; return b(a) end)() end)")
+                    t.assert_equals(clue.compiler.compile(ns, "(let [a a a a a b b a] (b a)"), "(function() local a = clue.namespaces[\"user.ns\"][\"a\"]; local a = a; local a = clue.namespaces[\"user.ns\"][\"b\"]; local b = a; return b(a) end)()")
                 end
             }
         }
