c6fd5d1e505dcf688216018fc49f2f74f7e5b7e1
Rafal Przywarski <rafal.przywarski@gmail.com>
2016-12-11 16:15:24 +0100

    removed redundant read_file functions

--- lua/compiler_test.lua
+++ lua/compiler_test.lua
@@ -5,15 +5,7 @@ require("compiler")
 local compile = clue.compiler.compile
 local read = function(e) return clue.reader.read(e):at(0) end
 
-local function read_file(path)
-    local file = io.open(path, "rb")
-    if not file then return nil end
-    local content = file:read("*a")
-    file:close()
-    return content
-end
-
-loadstring(clue.compiler.compile(read_file("clue/core.clu")))()
+loadstring(clue.compiler.compile_file("clue/core.clu"))()
 t.describe("clue.compiler", {
     ["before each"] = function()
         t.save_global("clue.namespaces")
--- lua/repl.lua
+++ lua/repl.lua
@@ -1,14 +1,6 @@
 require "compiler"
 
-local function read_file(path)
-    local file = io.open(path, "rb")
-    if not file then return nil end
-    local content = file:read("*a")
-    file:close()
-    return content
-end
-
-loadstring(clue.compiler.compile(read_file("clue/core.clu")))()
+loadstring(clue.compiler.compile_file("clue/core.clu"))()
 clue.ns("user")
 
 local cmd
